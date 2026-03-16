6a1e6f930266ee7b671d4c4df073d75d8bbd2e62
loktioncode <elishabere4@gmail.com>
2026-03-16 15:26:37 +0200

    style: Update global styles for light theme and enhance input component with neutral styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
